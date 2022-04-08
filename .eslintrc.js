37dd2c1c5fd3fa82af5e9909d6d9ad251f40b1f3
wxy <102939294+noBaldAaa@users.noreply.github.com>
2022-04-08 11:12:39 +0800

    feat: test

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,13 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
     ecmaFeatures: {
       jsx: true,
     },
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,7 @@ export * from "./useMount";
 export * from "./useUnMount";
 export * from "./useUpdateEffect";
 export * from "./useFirstMountState";
+
+const a: string[] = 1;
+a.map((v) => v);
+console.log(a);
