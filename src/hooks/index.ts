c51b52d90fdaa0f63a847521059aa7b74512970c
wxy <102939294+noBaldAaa@users.noreply.github.com>
2022-04-08 11:35:26 +0800

    feat: test

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,7 +3,3 @@ export * from "./useMount";
 export * from "./useUnMount";
 export * from "./useUpdateEffect";
 export * from "./useFirstMountState";
-
-let a: string[] = 1;
-a = a + "111";
-console.log(a);
